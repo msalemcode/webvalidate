de70a9d49ce56e8cfab2176fa25b8de71d3c7fa4
bartr <bartr@microsoft.com>
2020-01-25 11:27:43 -0600

    Initial commit from helium

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Change Log
+
+## V1.0.0
+
+### Jan 31, 2020
+
+- Split from Helium repo
---
+++ src/Version.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Helium
+{
+    /// <summary>
+    /// Assembly Versioning
+    /// </summary>
+    public sealed class Version
+    {
+        static string _version = string.Empty;
+
+        public static string AssemblyVersion
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_version))
+                {
+                    string file = System.Reflection.Assembly.GetExecutingAssembly().Location;
+                    DateTime dt = System.IO.File.GetCreationTime(file);
+
+                    System.Version aVer = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+
+                    _version = string.Format(CultureInfo.InvariantCulture, $"{aVer.Major}.{aVer.Minor}.{dt.ToString("MMdd.HHmm", CultureInfo.InvariantCulture)}");
+                }
+
+                return _version;
+            }
+        }
+    }
+}
